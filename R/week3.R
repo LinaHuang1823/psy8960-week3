877cab0d89715cc134b863f611f655b1639b4952
Lina Huang <huan2384@umn.edu>
2023-02-03 10:10:49 -0600

    update clean_df

--- R/week3.R
+++ R/week3.R
@@ -5,11 +5,11 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 raw_df<-read.csv("data/week3.csv")
 raw_df$timeStart<-as.POSIXct(raw_df$timeStart,format = "%Y-%m-%d %H:%M:%S")
 raw_df$timeEnd<-as.POSIXct(raw_df$timeEnd,format = "%Y-%m-%d %H:%M:%S")
-clean_df<-raw_df[-(1:6),]
+clean_df<-raw_df[!grepl("2017-06", raw_df$timeStart), ]
 clean_df<-clean_df[clean_df$q6 == 1,]
 
 #Analysis
-clean_df$timeSpent<-difftime(clean_df$timeEnd, clean_df$timeStart,units = "secs")
+clean_df$timeSpent <-difftime(clean_df$timeEnd, clean_df$timeStart,units = "secs")
 hist(clean_df$timeSpent<-as.numeric(clean_df$timeSpent, units = "secs"))
 frequency_tables_list <-lapply(clean_df[,5:14], table)
 lapply(frequency_tables_list,barplot)
