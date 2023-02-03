a024cf02966004687bcea63240f6da2612181a24
Lina Huang <huan2384@umn.edu>
2023-02-02 22:05:41 -0600

    histogram

--- R/week3.R
+++ R/week3.R
@@ -7,3 +7,7 @@ raw_df$timeStart<-as.POSIXct(raw_df$timeStart,format = "%Y-%m-%d %H:%M:%S")
 raw_df$timeEnd<-as.POSIXct(raw_df$timeEnd,format = "%Y-%m-%d %H:%M:%S")
 clean_df<-raw_df[-(1:6),]
 clean_df<-clean_df[clean_df$q6 == 1,]
+
+#Analysis
+clean_df$timeSpent<-difftime(clean_df$timeEnd, clean_df$timeStart,units = "secs")
+hist(clean_df$timeSpent<-as.numeric(clean_df$timeSpent, units = "secs"))
